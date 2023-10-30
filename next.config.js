38302e175cc44edf095afe11433fc2b18af62b04
RonnieTan <58020839+RonnieTan@users.noreply.github.com>
2023-10-30 15:54:39 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/sensory-feedback",
+  basePath: "/image-captioning",
 
   /**
    * Disable server-based image optimization. Next.js does not support
